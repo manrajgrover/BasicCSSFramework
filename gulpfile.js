233c752da79aa21fc9d7a302b506bbf7bd1549ae
Manraj Singh <manrajsinghgrover@gmail.com>
2016-11-07 09:48:00 +0530

    Update: Intial updates for micro framework

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,20 @@
  
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const minifyCSS = require('gulp-minify-css');
+const concat = require('gulp-concat');
  
 gulp.task('sass', () => {
-  return gulp.src('./src/sass/*.scss')
+
+  return gulp.src('./src/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(minifyCSS({keepSpecialComments : 0}))
+    .pipe(concat('framework.css'))
+    .pipe(gulp.dest('./dist/'));
 });
  
 gulp.task('watch', () => {
-  gulp.watch('src/sass/*.scss', ['sass']);
+  gulp.watch('src/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass']);
